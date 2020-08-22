a500c8a696e0357bed863c720de21b573e20a860
Linh Nguyen Doan Thao <linhndt1911@gmail.com>
2020-08-23 02:34:06 +0700

    first commit

---
+++ ILS-TSP-BRA.py
@@ -0,0 +1,85 @@
+import numpy as np
+import time
+from SharedBRA import tourCost, stochasticTwoOpt, constructInitialSolution, getBRA
+
+
+def load_data(file):
+    data = np.loadtxt(file, skiprows=6)
+    data = np.delete(data, 0, 1).tolist()
+    return data
+
+
+def perturbation(aSol):
+    # uses a 'double bridge move' for perturbation
+    newSol = doubleBridgeMove(aSol)
+    newSolCost = tourCost(newSol)
+    return newSol, newSolCost
+
+
+# the double bridge move involves partitioning a permutation into 4 pieces
+# (a, b, c, d) and putting it back together in a specific and jumbled ording
+# (a, b, c, d) - this is equivalent to a 4-opt move
+def doubleBridgeMove(perm):
+    # make four slices
+    sliceLength = len(perm) / 4
+    p1 = 1 + getBRA(0.2, sliceLength)
+    p2 = p1 + 1 + getBRA(0.2, sliceLength)
+    p3 = p2 + 1 + getBRA(0.2, sliceLength)
+    # combine first and fourth slice in order
+    # combine third and second slice in order
+    # return the combination of the above two combined slices
+    return perm[0:p1] + perm[p3:] + perm[p2:p3] + perm[p1:p2]
+
+
+def localSearch(aSol, aCost, maxIter):
+    while maxIter > 0:
+        maxIter -= 1
+        newSol = stochasticTwoOpt(aSol)
+        newCost = tourCost(newSol)
+        if newCost < aCost:
+            aSol = newSol
+            aCost = newCost
+    return aSol, aCost
+
+
+if __name__ == "__main__":
+    """ALGORITHM FRAMEWORK"""
+    algorithmName = "ILSBRA"
+    file = "wi29.txt"
+    print("Best sol by" + algorithmName + "...")
+
+    # problem configuration
+    inputsTSP = load_data(file)
+    maxIteration = 15000
+    maxNoImprove = 100
+
+
+    cost = 0
+    time_ = 0
+    for i in range(0, 5):
+        start = time.perf_counter()
+        maxIteration = 15000
+        maxNoImprove = 100
+        # 1. generate an initial solution. we use a random permutation as the initial solution
+        bestSol = constructInitialSolution(inputsTSP)
+        bestCost = tourCost(bestSol)
+        # 2. refine this using a local search for getting to the local optima
+        bestSol, bestCost = localSearch(bestSol, bestCost, maxNoImprove)
+        # 3. iterate
+        while maxIteration > 0:
+            maxIteration -= 1
+            newSol, newCost = perturbation(bestSol)
+            newSol, newCost = localSearch(newSol, newCost, maxNoImprove)
+            if newCost < bestCost:
+                bestSol = newSol
+                bestCost = newCost
+                # print(bestCost, maxIteration)
+        # 4. stop clock and return outputs
+        stop = time.perf_counter()
+        print(bestCost)
+        cost += bestCost
+        time_ += (stop - start)
+    with open(algorithmName + "_" + file, "w") as out:
+        out.write("Best sol by" + algorithmName + "... \n")
+        out.write("Average Cost = {}\n".format(cost / 5))
+        out.write("Average Elapsed = {}\n ".format(time_ / 5))
---
+++ SharedBRA.py
@@ -0,0 +1,84 @@
+import math
+import random
+import numpy as np 
+
+
+berlin52 = np.loadtxt('berlin52.txt', skiprows=6)
+berlin52 = np.delete(berlin52, 0, 1).tolist()
+
+# evaluates the total length of a TSP solution (permutation of nodes)
+def tourCost(perm):
+    # is the sum of the Euclidean distance between consecutive points in the path
+    totalDistance = 0.0
+    size = len(perm)
+    for index in range(size):
+        startNode = perm[index]
+        # select the end point for calculating the segment length
+        if index == size - 1:
+            # in order to complete the 'tour' we need to reach the starting point
+            endNode = perm[0]
+        else:
+            endNode = perm[index + 1]
+
+        totalDistance += euclideanDistance(startNode, endNode)
+    return totalDistance
+
+
+# calculates the euclidean distance between two points
+def euclideanDistance(xNode, yNode):
+    sum = 0.0
+    # use Zip to iterate over the two vectors (nodes)
+    for xi, yi in zip(xNode, yNode):
+        sum += pow((xi - yi), 2)
+    return math.sqrt(sum)
+
+
+def getBRA(beta, size):
+    t1 = random.random()
+    index = math.log(t1) / math.log(1 - beta)
+    index = int(index) % size
+    return int(index)
+
+
+# deletes two edges and reverse the sequence in between the deleted edges
+def stochasticTwoOpt(perm):
+    result = perm[:]  # to avoide changing the original sol (perm), make a copy
+    size = len(result)
+    # select indices of two random points in the tour
+    p1, p2 = getBRA(0.2, size), getBRA(0.2, size)
+    # do this as not to overshoot tour boundaries
+    exclude = set([p1])
+    if p1 == 0:
+        exclude.add(size - 1)
+    else:
+        exclude.add(p1 - 1)
+
+    if p1 == size - 1:
+        exclude.add(0)
+    else:
+        exclude.add(p1 + 1)
+
+    while p2 in exclude:
+        p2 = getBRA(0.2, size)
+
+    # to ensure we always have p1 < p2
+    if p2 < p1:
+        p1, p2 = p2, p1
+    
+    # now reverse the tour segment between p1 and p2
+    result[p1:p2] = reversed(result[p1:p2])
+
+    return result
+
+
+# creates a random sol (permutation) from an initial permutation by shuffling its elements
+def constructInitialSolution(initPerm):
+    # randomise the initial permutation
+    permutation = initPerm[:]  # make a copy of the initial permutation
+    size = len(permutation)
+    for index in range(size):
+        # shuffle the values of the initial permytation randomly
+        # get a random index and exchange values
+        shuffleIndex = random.randrange(index, size)  # randange excludes upper bound
+        permutation[shuffleIndex], permutation[index] = permutation[index], permutation[shuffleIndex]
+    return permutation
